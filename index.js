b0c715158eac2ab37c4e703fe003a82a4357f7a3
gowrishankarsrinivasan <gowrishankar31032004@gmail.com>
2024-08-16 21:24:11 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const authRoutes = require('./routes/auth');
+const app = express();
+const port = 3000;
+const url = process.env.MONGODB_URL;
+
+
+mongoose.connect(url).then(() => {
+    console.log('connected to mongodb');
+}).catch(err => console.error(err));
+
+app.use(express.json());
+app.use('/api/auth', authRoutes)
+
+app.listen(port, () => {
+    console.log(`Server running on localhost ${port}`);
+});
---
+++ routes/auth.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const jwt = require('jsonwebtoken');
+const User = require('../models/users');
+
+const router = express.Router();
+
+// Register
+router.post('/register', async (req, res) => {
+    try {
+        const { username, password } = req.body;
+        const user = new User({ username, password });
+        await user.save();
+        res.status(201).send('User registered');
+    } catch (error) {
+        res.status(400).send(error.message);
+    }
+});
+
+// Login
+router.post('/login', async (req, res) => {
+    try {
+        const { username, password } = req.body;
+        const user = await User.findOne({ username });
+        if (!user || !(await user.comparePassword(password))) {
+            return res.status(401).send('Invalid credentials');
+        }
+
+        // Create a JWT token
+        const token = jwt.sign({ id: user._id }, 'your_jwt_secret', { expiresIn: '1h' });
+        res.json({ token });
+    } catch (error) {
+        res.status(400).send(error.message);
+    }
+});
+
+module.exports = router;
